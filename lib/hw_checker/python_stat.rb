1439210e413fa8e0e60c408d103c5a47263d1ade
Misha.Varenitsya <user@ubuntu.(none)>
2013-05-28 05:37:44 +0300

    added python test runner

--- lib/hw_checker/python_stat.rb
+++ lib/hw_checker/python_stat.rb
@@ -1,6 +1,26 @@
 module HomeWorkChecker
   module TestRunStat
     class PythonStat
+      def initialize(tmp_path, dirname)
+        @tmp_path, @dirname= tmp_path, dirname
+        @lines_all = @lines_failed = 0
+      end
+
+      def perform
+        Dir.foreach("#{@tmp_path}/#{@dirname}") do |p|
+          next unless File::file?("#{@tmp_path}/#{@dirname}/#{p}") && File.extname(p) == '.py'
+          `pylint #{@tmp_path}/#{@dirname}/#{p} > #{@tmp_path}/#{@dirname}/#{p}.tmp  2>&1`
+        end
+        calc_percent_quality
+      end
+
+      def calc_percent_quality(filename)
+        test_line = '' ; res = '' ; rake = 0
+        test_line = `tail -n 2 result.txt`
+        res = test_line.scan(/\d{1,2}/)
+        rake = res.shift.to_f
+        (( rake/10 ) * 100 )
+      end
     end
   end
 end
--- lib/hw_checker/python_test_run.rb
+++ lib/hw_checker/python_test_run.rb
@@ -1,9 +1,31 @@
 module HomeWorkChecker
   module TestRunStat
     class PythonTestRun
-      def initialize(path)
-        `cd #{path}/test`
-        puts `nosetests`
+      def initialize(tmp_path, dirname)
+        @tmp_path, @dirname= tmp_path, dirname
+        @passed = @failed = 0
+      end
+
+      def perform
+        Dir.foreach("#{@tmp_path}/#{@dirname}/test/application") do |p|
+          next unless File.file?("#{@tmp_path}/#{@dirname}/test/application/#{p}") && File.extname(p) == '.py'
+          `python #{@tmp_path}/#{@dirname}/test/application/#{p} > #{@tmp_path}/#{@dirname}/test/#{p}.tmp`
+          count_passed_failed("#{@tmp_path}/#{@dirname}/test/#{p}.tmp")
+        end
+        calc_percent_passed
+      end
+
+      private
+      def count_passed_failed(filename)
+        report.each_char do |value|
+          @passed += 1 if value == '.'
+          @failed += 1 if (value == 'F' || value == 'E')
+        end
+      end
+
+      def calc_percent_passed
+        return "0.00%" if @passed.zero? && @failed.zero?
+        '%.2f%%' % (@passed.to_f / (@passed + @failed) * 100).round(2)
       end
     end
   end
