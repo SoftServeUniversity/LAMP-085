c8b49558b2fb41297052e6c655b37fb258c39d84
Pavlo Petryk <pavlo_petryk@ukr.net>
2013-06-05 23:35:36 +0300

    project integrated

--- railsapp/app/models/report.rb
+++ railsapp/app/models/report.rb
@@ -1,5 +1,7 @@
 class Report < ActiveRecord::Base
   WORK_PATH = File.expand_path('app/received')
+  ARCHIVE_PASSED = ' have successfully processed'
+  ARCHIVE_FAILED = ' have failed. Something went wrong' 
 
   attr_accessible :homework_id, :quality, :ratio, :student_id, :time
   belongs_to :student
@@ -16,8 +18,45 @@ class Report < ActiveRecord::Base
 
   def self.home_work_check
     HomeWorkChecker::FileScan.new(WORK_PATH, '/tmp').each do |name, type|
-      Resque.enqueue(UnarchiveProcesor, WORK_PATH, '/tmp', name, type)
+      Resque.enqueue(ArchiveChecker, WORK_PATH, '/tmp', name, type)
     end
-  end 
+  end
 
+  def self.archive_passed(archive_name)
+    "Archive '#{archive_name}'#{ARCHIVE_PASSED}"
+  end
+
+  def self.archive_failed(archive_name)
+    "Archive '#{archive_name}'#{ARCHIVE_FAILED}"
+  end
+
+  def self.add_record(fn)
+    xml_content = xml_file_to_hash(fn)
+    s = Student.where(name: xml_content[:student]).first_or_create
+    h = Homework.where(
+      title: xml_content[:homework],
+      language: xml_content[:language]
+    ).first_or_create
+
+    s.reports.create(
+      homework_id: h.id,
+      ratio: xml_content[:ratio],
+      quality: xml_content[:quality],
+      time: xml_content[:time]
+    ).valid?
+  end
+
+  private
+  def self.xml_file_to_hash(fn)
+    Nokogiri::XML(File.open(fn, 'r')).xpath('//root').map do |f|
+      {
+        student: f.xpath('student').inner_text,
+        homework: f.xpath('homework').inner_text,
+        language: f.xpath('language').inner_text,
+        ratio: f.xpath('ratio').inner_text.to_f,
+        quality: f.xpath('quality').inner_text.to_f,
+        time: f.xpath('time').inner_text.to_f
+      }
+    end.first
+  end
 end
