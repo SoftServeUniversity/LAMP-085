aec154196a15004fcc21d47467441cdf86615df9
Red4ik <red4ik@gmail.com>
2013-06-11 11:34:33 +0300

    tests for student table

--- rails_app/spec/models/student_spec.rb
+++ rails_app/spec/models/student_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Student do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :reports }
+  it { should have_many(:homeworks).through(:reports) }
+
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:name).with_message(/^[a-z]+.[a-z]+$/i) }
 end
