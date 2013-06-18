932ad5dba8bb1b15e2682ebdeb55e1b564a05582
Andriy Baran <andriy.baran.v@gmail.com>
2013-06-18 12:54:14 +0300

    abstract class for new lenguages

---
+++ ruby_cmd/lib/hw_checker/abstract.rb
@@ -0,0 +1,25 @@
+module HomeWorkChecker
+  module TestRunStat
+    
+    class TestQ
+      def intitilize
+        raise "Don't use methods of abstract class"
+      end
+
+      def perform
+        raise "Don't use methods of abstract class"
+      end
+    end
+
+    class TestR
+      def intitilize
+        raise "Don't use methods of abstract class"
+      end
+
+      def perform
+        raise "Don't use methods of abstract class"
+      end
+    end
+    
+  end
+end
